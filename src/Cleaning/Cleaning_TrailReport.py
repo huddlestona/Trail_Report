535d4e411106586eef090a2e2ca6fc50952db347
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-09 15:34:50 -0700

    build main block and clean function info

--- src/Cleaning/Cleaning_TrailReport.py
+++ src/Cleaning/Cleaning_TrailReport.py
@@ -10,21 +10,31 @@ trail_reports = db['trail_reports']
 raw_html = db['html']
 trail_page_raw_html = db['trail_html']
 
-df_reports = pd.DataFrame(list(trail_reports.find()))
-df_reports['Creator'] = df_reports['Creator'].apply(lambda x: x.strip('\n'))
-df_reports['Date_type'] = pd.to_datetime(df_reports['Date'])
-df_reports['conditions_split'] = df_reports[df_reports['Trail_condtions'].notna()]['Trail_condtions'].apply(lambda x: split_conditions(x))
 
 def split_conditions(string):
     conditions = string.split('\n')[2].split(',')
     con_clean = [con.strip(' ') for con in conditions]
     return con_clean
 
-conditions = ['snow','trail','bugs', 'road']
-for cond in conditions:
-    df_reports[f"condition|{cond}"] = df_reports['conditions_split'].apply(lambda x: cond in str(x))
+def condition_dummies(df):
+    conditions = ['snow','trail','bugs', 'road']
+    for cond in conditions:
+        df[f"condition|{cond}"] = df['conditions_split'].apply(lambda x: cond in str(x))
 
-trail_dummies = pd.get_dummies(df_reports['Trail'])
-df_all = pd.concat([df_reports,trail_dummies], axis=1)
+def clean_trailreport(df):
+    df['Creator'] = df ['Creator'].apply(lambda x: x.strip('\n'))
+    df['Date_type'] = pd.to_datetime(df['Date'])
+    df['conditions_split'] = df[df['Trail_condtions'].notna()]['Trail_condtions'].apply(lambda x: split_conditions(x))
+    condition_dummies(df)
+    trail_dummies = pd.get_dummies(df['Trail'])
+    df_all = pd.concat([df,trail_dummies], axis=1)
+    return df_all.drop(['conditions_split','Date','Trail_condtions','Trail'], axis=1)
 
-df_clean = df_all.drop(['conditions_split','Date','Trail_condtions','Trail'], axis=1)
+if __name__ == '__main__':
+    mc = pymongo.MongoClient()
+    db = mc['wta']
+    trail_reports = db['trail_reports']
+    df = pd.DataFrame(list(trail_reports.find()))
+    clean__reports_df = clean_trailreport(df)
+    clean__reports_df.to_csv('../../data/WTA_olympics_trailreports_clean.csv')
+    
