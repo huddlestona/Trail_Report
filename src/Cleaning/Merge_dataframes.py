f557e74bcacc981ebfb2841a6723604444739645
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-09 16:56:57 -0700

    build main block

--- src/Cleaning/Merge_dataframes.py
+++ src/Cleaning/Merge_dataframes.py
@@ -7,7 +7,14 @@ df_report = pd.read_csv('data/WTA_olympics_trailreports_clean.csv', sep = '|', l
 
 def merge_files(df_trail,df_report):
     """ This function left joins trails and reports, adding trail data to the report you built"""
+    df_reports = df_report.drop('Unnamed: 0',axis =1)
+    df_trails = df_trail.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1)
+    merged = pd.merge(df_reports, df_trails, left_on='Trail', right_on='hike_name', how='left', sort=False)
+    return merged
+
+if __name__ == '__main__':
+    df_trail = pd.read_csv('data/Olympics_189hike_data.csv')
+    df_report = pd.read_csv('data/WTA_olympics_trailreports_clean.csv', sep = '|', lineterminator='\n')
+    merged_df = merge_files(df_trail,df_report)
+    merged_df.to_csv('../../data/WTA_olympics_allmerged.csv', sep = '|')
     
-df_reports = df_report.drop('Unnamed: 0',axis =1)
-df_trails = df_trail.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1)
-result = pd.merge(df_reports, df_trails, left_on='Trail', right_on='hike_name', how='left', sort=False)
