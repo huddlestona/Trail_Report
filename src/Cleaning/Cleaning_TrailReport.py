9f0b8cc76fb269abc9309bd0fbe7e1d19bcc61f4
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-12 18:40:09 -0700

    add year and dummy variables

--- src/Cleaning/Cleaning_TrailReport.py
+++ src/Cleaning/Cleaning_TrailReport.py
@@ -5,24 +5,34 @@ import pandas as pd
 import numpy as np
 
 def split_conditions(string):
-    conditions = string.split('\n')[2].split(',')
-    con_clean = [con.strip(' ') for con in conditions]
+    conditions= string.split('\n')[2].split(',')
+    con_clean= [con.strip(' ') for con in conditions]
     return con_clean
 
 def condition_dummies(df):
-    conditions = ['snow','trail','bugs', 'road']
+    full_conditions= df[df['Trail_condtions'].notna()]['Trail_condtions']
+    df['conditions_split'] = full_conditions.apply(lambda x: split_conditions(x))
+    conditions= ['snow','trail','bugs', 'road']
     for cond in conditions:
         df[f"condition|{cond}"] = df['conditions_split'].apply(lambda x: cond in str(x))
 
 def clean_trailreport(df):
-    df['Creator'] = df ['Creator'].apply(lambda x: x.strip('\n'))
-    df['Date_type'] = pd.to_datetime(df['Date'])
-    full_conditions = df[df['Trail_condtions'].notna()]['Trail_condtions']
-    df['conditions_split'] = full_conditions.apply(lambda x: split_conditions(x))
+    df['Creator']= df ['Creator'].apply(lambda x: x.strip('\n'))
+    df['Date']= pd.to_datetime(df['Date'])
+    df['last_year']= df['Date'].apply( lambda x: x.year-1)
+    df['last_month']= df['Date'].apply( lambda x: x.year-1)
+    df['month'] = df['Datetime'].apply( lambda x: x.month + x.year)
+    df['year'] = df['Datetime'].apply( lambda x: x.year)
+    df['monthyear'] = df['Datetime'].apply( lambda x: str(x.month)+'-'+str(x.year))
     condition_dummies(df)
-    trail_dummies = pd.get_dummies(df['Trail'])
-    df_all = pd.concat([df,trail_dummies], axis=1)
-    return df_all.drop(['conditions_split','Date','Trail_condtions'], axis=1)
+    trail_dummies= pd.get_dummies(df['Trail'])
+    month_dum = pd.get_dummies(df['month'])
+    year_dum = pd.get_dummies(df['year'])
+    monthyear_dummies = pd.get_dummies(df['monthyear'])
+    pass_dummies = pd.get_dummies(df['which_pass'])
+    subregion_dummies = pd.get_dummies(df['sub_region'])
+    df_all = pd.concat([df,trail_dummies,monthyear_dummies,pass_dummies,subregion_dummies], axis=1)
+    return df_all.drop(['conditions_split','Trail_condtions'], axis=1)
 
 if __name__ == '__main__':
     mc = pymongo.MongoClient()
