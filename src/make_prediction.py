167e7dac0356636edcb9d4d6459ecce04f7871d5
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-15 15:22:22 -0700

    make get_new_neighbors

--- src/make_prediction.py
+++ src/make_prediction.py
@@ -1,14 +1,29 @@
+from knn_model import *
+from Cleaning.Merge_Weather import *
+
+def get_new_neighbors(df,hike,date,condition):
+    neigh = prep_neighbors(df,condition)
+    hike_df = df.loc[df['Trail'] == hike][['highest_point','distance_from_median']]
+    hike_df['month'] = date.month
+    hike_info = hike_df.iloc[0]
+    X = scale(hike_info)
+    indx = neigh.kneighbors([X])
+    indx_list = list(indx[1][0])
+    neighbors = df.iloc[indx_list]
+    average = neighbors[condition]
+    print(hike,date,condition)
+    return neighbors
 
 def get_hike_info(hike,df):
-    conidtion = 'condition|snow'
+    condition = 'condition|snow'
     hike_df = df.loc[df['Trail'] == hike]
     df_clean = prep_for_knn(hike_df)
-    test = df_clean[df_clean['year'] >= year]
-    get_neighbors(test,conditon)
+    test = df_clean[df_clean['year'] >= 2017]
     return test
 
 def prep_input_data(df_test):
-    conidtion = 'condition|snow'
+    condition = 'condition|snow'
+    conditions = ['condition|snow', 'condition|trail','condition|bugs','condition|road']
     drop_list = conditions+['last_year','year',
     'station_distance','closet_station']
     test_X = df_test.drop(drop_list, axis = 1)
@@ -20,8 +35,9 @@ if __name__ == '__main__':
     df_weather,df_weather_dist = get_weather_data()
     hike = input("Where would you like to hike? ")
     hike_date = input("When do you want to go? ")
+    date = date = pd.to_datetime(hike_date)
     test = get_hike_info(hike,df)
     get_closest_station(test,df_weather_dist)
     df_test = merge_weather_trails(df_weather,test)
-    test_X = prep_input_data(df_test)    
+    test_X = prep_input_data(df_test)
     print (f"There is a perc. chance of snow on {hike_date} at {hike}")
