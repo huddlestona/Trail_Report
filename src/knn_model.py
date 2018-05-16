9157362135196f05da60870e9442f2e193cd6078
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-15 18:22:26 -0700

    edit input

--- src/knn_model.py
+++ src/knn_model.py
@@ -1,4 +1,4 @@
-from Cleaning.Merge_Weather import import_weather, merge_weather_trails, get_closest_station
+from Cleaning.Merge_Weather import get_weather_data, merge_weather_trails, get_closest_station
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
@@ -14,9 +14,9 @@ conditions = ['condition|snow', 'condition|trail','condition|bugs','condition|ro
 
 def prep_for_knn(df):
     df_new = df.drop(['Unnamed: 0','Unnamed: 0_x','Creator','Trail','Report',
-    'Votes','_id','last_month','month','Unnamed: 0_y','hike_name','url',
+    'Votes','_id','Unnamed: 0_y','hike_name','url',
 
-    'super_region','sub_region','monthyear','which_pass'], axis=1)
+    'super_region','sub_region','which_pass'], axis=1)
     df_new['Date'] = pd.to_datetime(df_new['Date'])
     df_new['month'] = df_new['Date'].apply(lambda x: x.month)
     df_full = df_new.fillna(0)
@@ -27,13 +27,16 @@ def train_test_split(df,year):
     train = df[df['year'] < year]
     return test,train
 
-def get_neighbors(df,condition):
+def prep_neighbors(df,condition):
     neigh = KNeighborsClassifier(n_neighbors=20)
     X = df[['highest_point','distance_from_median','month']]
     y = df[condition]
     y = y.astype(bool)
-    X_s = normalize(scale(X))
+    X_s = scale(X)
     neigh.fit(X_s,y)
+    return neigh
+
+def get_neighbors(neigh,df,condition):
     all_n = neigh.kneighbors()
     averages = []
     for idx_neighbors in all_n[1]:
@@ -42,16 +45,17 @@ def get_neighbors(df,condition):
     df[f'neighbors_average {condition}'] = averages
 
 def add_cols(test,train, df_weather_dist,condition):
-    get_neighbors(test,condition)
-    get_neighbors(train, condition)
+    neigh_train = prep_neighbors(train, condition)
+    neigh_test = prep_neighbors(train, condition)
+    get_neighbors(neigh_test,test,condition)
+    get_neighbors(neigh_train,train, condition)
     get_closest_station(train,df_weather_dist)
     get_closest_station(test,df_weather_dist)
 
 def get_knn_inputs(test,train,condition):
     conditions = ['condition|snow', 'condition|trail','condition|bugs','condition|road']
     test_y = test[condition]
-    drop_list = conditions+['last_year','year',
-    'station_distance','closet_station']
+    drop_list = conditions+['year','closet_station']
     test_X = test.drop(drop_list, axis = 1)
     train_y = train[condition]
     train_X = train.drop(drop_list, axis = 1)
@@ -70,15 +74,12 @@ if __name__ == '__main__':
     df = pd.read_csv('../data/new_olympics_merged.csv', sep = '|',lineterminator='\n')
     df_clean = prep_for_knn(df)
     test,train = train_test_split(df_clean,2016)
-    #get weather data
-    keys = ['Global_sum_FIPS:53031 FIPS:53009.csv','Global_sum_FIPS:53045 FIPS:53027.csv']
-    df_weather = import_weather(keys)
-    df_weather_dist = df_weather[['LATITUDE','LONGITUDE','name']].drop_duplicates().reset_index()
-    #add feature columns
+    df_weather,df_weather_dist = get_weather_data()
     add_cols(test,train, df_weather_dist)
     #merge and save full df
     df_test = merge_weather_trails(df_weather,test)
     df_train = merge_weather_trails(df_weather,train)
     train_X,train_y,test_X,test_y = get_knn_inputs(df_test,df_train)
+
     model,pred = make_forest(train_X,train_y,test_X,test_y)
     print (f'final predictions {pred}')
