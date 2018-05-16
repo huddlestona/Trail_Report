5aa9690ca57c34b9ea1e31367048a1a898a0a02e
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-16 16:56:43 -0700

    make file

---
+++ src/website/web_base.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#home page
+
+@app.route('/')
+def index():
+  return '''
+    <!DOCTYPE html>
+    <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Trip Report</title>
+            </head>
+        <body>
+            <!--page content -->
+            <h1> Determine the conditions of a new trail in Washington</h1>
+            <p>
+                Trail:
+                Date:
+                </p>
+        </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
