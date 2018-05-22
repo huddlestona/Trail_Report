9d5a491037a73ea9b5344579cfb4c9bc3578f63e
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-21 22:18:13 -0700

    change return labels

--- src/startbootstrap-agency/app.py
+++ src/startbootstrap-agency/app.py
@@ -16,16 +16,16 @@ def index():
 def solve():
     user_data = request.json
     hike, date = user_data['hike'], user_data['hike_date']
-    root_1,root_2,root_3,root_4= _get_prediction(hike, date)
-    return jsonify({'root_1': root_1,'root_2': root_2,'root_3': root_3,'root_4': root_4})
+    snow_prob,trail_prob,bugs_prob,road_prob = _get_prediction(hike, date)
+    return jsonify({'snow_prob': snow_prob,'trail_prob': trail_prob,'bugs_prob': bugs_prob,'road_prob': road_prob})
 
 
 def _get_prediction(hike, date):
     X_test = get_data(hike,date)
     pred = tp.predict(X_test)
-    return (pred['condition|snow'][:,1][0],
-            pred['condition|trail'][:,1][0],
-            pred['condition|bugs'][:,1][0],
-            pred['condition|road'][:,1][0])
+    return ("{0:.0f}".format(float(pred['condition|snow'][:,1][0])*100),
+            "{0:.0f}".format(float(pred['condition|trail'][:,1][0])*100),
+            "{0:.0f}".format(float(pred['condition|bugs'][:,1][0])*100),
+            "{0:.0f}".format(float(pred['condition|road'][:,1][0])*100))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
