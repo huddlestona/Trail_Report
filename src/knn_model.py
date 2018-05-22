28fd60c0312253e51bd22f1b6f4e4a9708959c06
Ashley Huddleston <ash.hudd@gmail.com>
2018-05-22 12:27:42 -0700

    change make forest

--- src/knn_model.py
+++ src/knn_model.py
@@ -72,12 +72,11 @@ def get_neighbors(neigh,df,condition):
         averages.append(neighbors[condition].mean())
     df[f'neighbors_average {condition}'] = averages
 
-def make_forest(X_train,y_train,X_test):
+def make_forest(X_train,y_train):
     """Runs RandomForestClassifier on prepped data."""
     model = RandomForestClassifier(n_estimators=500)
     fit = model.fit(X_train,y_train)
-    pred = model.predict_proba(X_test)
-    return model, pred
+    return fit
 
 def make_logistic(X_train,y_train):
     """Runs LogisticRegression on prepped data."""
