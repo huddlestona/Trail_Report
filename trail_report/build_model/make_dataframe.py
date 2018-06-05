3a06cba1d3374fce9b7106a437f10509e26aead3
Ashley Huddleston <ash.hudd@gmail.com>
2018-06-05 16:38:17 -0700

    save data from s3 for make_dataframe

--- trail_report/build_model/make_dataframe.py
+++ trail_report/build_model/make_dataframe.py
@@ -2,7 +2,7 @@
 This file, when called in the terminal, will add new features,merge
 in the weather dataframe, and save the final X and Y databases.
 """
-from Cleaning.Merge_Weather import get_weather_data, merge_weather_trails, get_closest_station
+from merge_weather import get_weather_data, merge_weather_trails, get_closest_station
 from knn_model import prep_for_knn,prep_neighbors,get_neighbors
 import pandas as pd
 
